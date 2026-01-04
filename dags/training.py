7b101d71219c3c3faeb87f4d4a1ab1869c140c3c
MathieuVReine <Mathieu.vincent.reine@gmail.com>
2026-01-04 18:38:35 +0000

    Ajout du dag de training et mise à jour du dag de preprocessing afin de le synchroniser avec la version du main

--- dags/training.py
+++ dags/training.py
@@ -13,35 +13,33 @@ import os
 CURRENT_DIR = os.getcwd()
 MINIO_USER = os.getenv("MINIO_ROOT_USER", "minio")
 MINIO_PASS = os.getenv("MINIO_ROOT_PASSWORD", "minio123")
-WORKDIR = os.getenv("WORKDIR", "/workspace")
+ML_FLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI", "http://mlflow:5000")
 DATA_VOLUME = "airflow_data"
 # Configuration commune pour éviter de répéter le code dans chaque tâche
 DOCKER_COMMON_ARGS = {
-    "image": "spacy:3.7.5",
+    "image": "sklearn:v1.7.2",  # Votre image locale
     "api_version": "auto",
-    "auto_remove": True,
+    "auto_remove": True,          # Nettoie le conteneur après exécution
     "force_pull":  False,
     "docker_url": "unix://var/run/docker.sock",
-    "network_mode": "mlflow-network",
+    "network_mode": "mlflow-network", # Pour parler à MinIO et MLflow
     "working_dir": "/workspace",  # Dossier de travail DANS le conteneur éphémère
     "mounts": [
         Mount(source=DATA_VOLUME, target="/workspace/data", type="volume"),
     ],
-    "user":"trainusr",
     "environment": {
         # Indispensable pour que python trouve le module 'src'
-        "WORKDIR": WORKDIR,
         "PYTHONPATH": "/",
         # Config MLflow & MinIO
-        "MINIO_HOST": "minio",
+        "MINIO_HOST": MINIO_USER,
         "MINIO_PORT": "9000",
-        "MLFLOW_TRACKING_URI": "http://mlflow:5000",
+        "MLFLOW_TRACKING_URI": ML_FLOW_TRACKING_URI,
         "MLFLOW_S3_ENDPOINT_URL": "http://minio:9000",
         "MINIO_ACCESS_KEY": MINIO_USER,
         "MINIO_SECRET_KEY": MINIO_PASS,
-        "AWS_ACCESS_KEY_ID": MINIO_USER,     
-        "AWS_SECRET_ACCESS_KEY": MINIO_PASS,
-        "PYTHONUNBUFFERED": "1"
+        "AWS_ACCESS_KEY_ID": MINIO_USER,
+        "AWS_SECRET_ACCESS_KEY": MINIO_PASS, 
+        "PYTHONUNBUFFERED": "1" # Pour voir les logs en direct dans Airflow
     }
 }
 
@@ -56,16 +54,16 @@ default_args = {
 }
 
 with DAG(
-    dag_id='preprocessing_pipeline',
+    dag_id='training_pipeline',
     default_args=default_args,
     schedule_interval=None, # Déclenchement manuel uniquement
     catchup=False,
-    tags=['mlops', 'rakuten', 'docker']
+    tags=['mlops', 'rakuten', 'docker', 'training']
 ) as dag:
 
     start = BashOperator(
         task_id='start_pipeline',
-        bash_command='echo "🚀 Démarrage du pipeline Rakuten"'
+        bash_command='echo "🚀 Démarrage du pipeline d\'entraînement Rakuten"'
     )
 
     init_volume = DockerOperator(
@@ -85,36 +83,19 @@ with DAG(
         auto_remove=True,
         docker_url="unix://var/run/docker.sock",
     )
+
     # --- Étape 1 : Récupérer les données (remplace DVC ou mc mirror)
     # On lance le script sync_bucket.py en mode PULL
     # Cela va télécharger le bucket 'raw' vers /workspace/data/raw
     fetch_data_task = DockerOperator(
-        task_id='fetch_raw_data',
-        command="sh -c 'python /src/utils/sync_bucket.py raw --mode pull && python /src/utils/sync_bucket.py dataset --mode pull'",
+        task_id='fetch_preprocessed_data',
+        command="python /src/utils/sync_bucket.py preprocessed --mode pull",
         **DOCKER_COMMON_ARGS
     ) 
 
-    # --- Étape 2 : Enrichissement du dataset ---
-    # Lit data/raw -> Écrit dans data/dataset (ou ailleurs selon votre logique)
-    enrich_task = DockerOperator(
-        task_id='enrich_dataset',
-        command="python /src/enrich_raw_dataset.py",
-        **DOCKER_COMMON_ARGS
-    )
-
-    # --- Étape 3 : Preprocessing ---
-    # Nettoyage, tokenization, préparation pour l'entraînement
-    preprocess_task = DockerOperator(
-        task_id='preprocessing',
-        command="python /src/preprocessing.py",
-        **DOCKER_COMMON_ARGS
-    )
-
-    # --- Étape 4 : Upload des résultats vers le bucket ---
-    # On envoie /workspace/data/dataset vers le bucket 'dataset'
-    upload_results_task = DockerOperator(
-        task_id='upload_results',
-        command="python /src/utils/sync_bucket.py dataset --mode push",
+    training_task = DockerOperator(
+        task_id='train_model',
+        command="python /src/train_text_model.py",
         **DOCKER_COMMON_ARGS
     )
 
@@ -126,5 +107,5 @@ with DAG(
     # =========================================================================
     # 🔗 ORCHESTRATION
     # =========================================================================
-    
-    start >> init_volume >> fetch_data_task >> enrich_task >> preprocess_task >> upload_results_task >> end
+
+    start >> init_volume >> fetch_data_task >> training_task >> end
