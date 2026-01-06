584404c3873d9ffd673c0757e42898a9f35d356f
Jean-Baptiste BILLAUD <billaud.jeanbaptiste@gmail.com>
2026-01-06 16:42:09 +0100

    ajout d'une image git/dvc; renommage des images et push vers docker hub (automatisé avec un script); uniformisation des paths dans les dockerfiles; mise à jour du compose (dockerhub + airflow 3).

---
+++ docker/docker-images-build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+# Dossier où se trouve ce script
+SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+
+# On remonte d’un niveau (parent de docker/)
+PROJECT_ROOT=$(CDPATH= cd -- "$SCRIPT_DIR/.." && pwd)
+
+cd "$PROJECT_ROOT"
+
+REPO=helianthus/rakuten
+
+build_push () {
+  echo "build $2 image and push"
+  docker buildx build -f $1 -t $REPO:$2 . --push
+  echo
+}
+
+build_push ./docker/api-gateway/Dockerfile fastapi-v0.115.0
+build_push ./docker/authentification/Dockerfile authentification
+build_push ./docker/bentoml/Dockerfile bentoml-v1.4.30
+build_push ./docker/drift-detector/Dockerfile evidently-v0.4.33
+build_push ./docker/dvc/Dockerfile dvc-v3.64.0
+build_push ./docker/minio-client/Dockerfile minio-client-RELEASE.2025-08-13T08-35-41Z
+build_push ./docker/mlflow/Dockerfile mlflow-v3.6.0
+build_push ./docker/spacy/Dockerfile spacy-v3.7.5
+build_push ./docker/trainer/Dockerfile sklearn-v1.7.2
--- docker/todo.md
+++ docker/todo.md
@@ -1,3 +1,10 @@
 - modifier les scripts services pour qu'ils utilisent les différents scripts de src montés dans les dockers:
   - preprocessing -> utilities: sera en charge de la manipulation des dataset et de la création du model dockérisé par bentoml
   - model -> à remplacer par le docker créé par bentoml
+
+- entrainement: récupérer hash du dernier commit des données et le passer en variable d'env au docker d'entrainement
+
+- initialisation du projet:
+  - branche github dédiée
+  - docker file dvc
+  - script pour builder image dvc, créer volume et télécharger le repo
